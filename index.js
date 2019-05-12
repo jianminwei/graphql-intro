e713d4437dc6c70707ce750eecd6cd13cfe4e4ad
Jianmin <jianminwei1@gmail.com>
2019-05-12 11:46:26 -0400

    finally was able to refactored the resolver to it's own index.js

--- index.js
+++ index.js
@@ -5,44 +5,7 @@ const books = require("./data/books")
 
 const typeDefs = fs.readFileSync('./typeDefs.graphql', 'UTF-8')
 
-// const resolvers = require('./resolvers')
-// const Query = require("./resolvers/Query");
-
-const resolvers = {
-    Query: {
-        allAuthors: (parent, args, context, info) => {
-            return authors
-        },
-
-        author(parent, args, context, info) {
-            return authors.find(author => author.id === args.id);
-        },
-
-        allBooks: (parent, args, context, info) => {
-            return books
-        },
-
-        book(parent, args, context, info) {
-            return books.find(book => book.id === args.id);
-        },
-    },
-
-    Author: {
-        books(parent, args, context, info) {
-            return books.filter(book => book.author === parent.id);
-        },
-    },
-
-    Book: {
-        author(parent, args, context, info) {
-            let result = authors.filter(author => author.id === parent.author);
-
-            //here we have to return a individual Author instead of a list.
-            return result[0];
-        },
-    },    
-};
-
+const resolvers = require('./resolvers')
 
 const context = { books, authors }
 
--- resolvers/Author.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    books(parent, args, context, info) {
-        return books.filter(book => book.author === parent.id);
-    },
-}
